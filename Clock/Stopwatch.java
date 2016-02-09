cc78eae6431fa6d1b8b262bbdc3e24d84649c3f1
JD9999 <jamiemdouglass@gmail.com>
2016-02-10 07:51:33 +1100

    Updated to V1.3, bug fix
    Added ChatColor import for player message, fixed a bug where the elapsed time would always be 0, moved TimerTask variable to the stopwatch.
    
    TODO: Remove startstopwatchplr(MC_Player plr) as it does not require the plr object, and does exactly the same as startstopwatch().

--- Clock/Stopwatch.java
+++ Clock/Stopwatch.java
@@ -4,41 +4,37 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import PluginReference.MC_Player;
+import PluginReference.ChatColor;
 
 public class Stopwatch{
 	public Stopwatch() {
 	}
 	public static boolean millisecond = false;
-	public static boolean stoptimer = false;
 	public int elapsedseconds = 0;
-	public int elapsedmilliseconds = 0;	
+	public int elapsedmilliseconds = 0;
+	public TimerTask task;
+	
 	public TimerTask startstopwatch(){
 		Timer timer = new Timer();
 		TimerTask task = new TimerTask(){
 			@Override
 			public void run() {
-				if(stoptimer){
-					System.out.println("The stopwatch finished at: " + elapsedmilliseconds + "milliseconds");
-					timer.cancel();
-				}
+				elapsedmilliseconds++;
 			}	
 		};
 		TimerTask task2 = new TimerTask(){
 			@Override
 			public void run() {
-				if(stoptimer){
-					System.out.println("The stopwatch finished at: " + elapsedseconds + "seconds");
-					timer.cancel();
-				}
+				elapsedseconds++;
 			}
 		};
 		if(millisecond){
 		timer.scheduleAtFixedRate(task, 1, 1);
-		MyPlugin.task = task;
+		this.task = task;
 		return task;
 		}else{
 		timer.scheduleAtFixedRate(task2, 1000, 1000);
-		MyPlugin.task = task2;
+		this.task = task2;
 		return task2;
 		}	
 	}
@@ -47,37 +43,41 @@ public class Stopwatch{
 		TimerTask task = new TimerTask(){
 			@Override
 			public void run() {
-				if(stoptimer){
-					System.out.println("The stopwatch finished at: " + elapsedmilliseconds + "milliseconds");
-					timer.cancel();
-				}
+				elapsedmilliseconds++;
 			}	
 		};
 		TimerTask task2 = new TimerTask(){
 			@Override
 			public void run() {
-				if(stoptimer){
-					plr.sendMessage("The stopwatch finished at: " + elapsedseconds + "seconds");
-					timer.cancel();
-				}
+				elapsedseconds++;
 			}
 		};
 		if(millisecond){
 		timer.scheduleAtFixedRate(task, 1, 1);
-		MyPlugin.task = task;
+		this.task = task;
 		return task;
 		}else{
 		timer.scheduleAtFixedRate(task2, 1000, 1000);
-		MyPlugin.task = task2;
+		this.task = task2;
 		return task2;
 		}	
 	}
 	public void stopstopwatch(){
 		System.out.println("Stopping timer!");
-		stoptimer = true;
+		if(millisecond){
+			System.out.println("Stopwatch finished at: " + elapsedmilliseconds + " milliseconds");
+		}else{
+			System.out.println("Stopwatch finished at: " + elapsedseconds + " seconds");
+		}
+		task.cancel();
 	}
 	public void stopstopwatchplr(MC_Player plr){
 		plr.sendMessage("Stopping timer!");
-		stoptimer = true;
+		if(millisecond){
+			plr.sendMessage(ChatColor.GOLD + "Stopwatch finished at: " + elapsedmilliseconds + " milliseconds");
+		}else{
+			plr.sendMessage(ChatColor.GOLD + "Stopwatch finished at: " + elapsedseconds + " seconds");
+		}
+		task.cancel();
 	}
 }
